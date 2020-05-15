bc75e27d3d46501d5f93cc0da25938d7fe11ee2a
unknown <handoo.r@northeastern.edu>
2020-05-15 15:27:46 -0400

    Adding Mocha for testing

---
+++ test/test-pages.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect
+const express = require('express')
+const app = express()
+it('Main page status', function (done) {
+  app.get('http://localhost:3000', (req, res) => {
+    expect(res.statusCode).to.equal(200)
+  }).then(done())
+})
