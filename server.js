349ac47fcf4f42515aeb351140b44fe0e65498ae
Archaengel <god11341258@gmail.com>
2020-05-23 15:46:27 -0700

    Refactor database connection logic to sense environment. Fix seed script so it terminates. Add database clear script

--- server.js
+++ server.js
@@ -4,80 +4,43 @@
  * @version 1.0
  * createdDate: 03/27/2020
  */
-
-const dotenv = require('dotenv')
-dotenv.config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const app = express()
-const cors = require('cors')
-const path = require('path')
-const CONSTANTS = require('./src/CONSTANTS/constants')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { connectDatabase } = require('./src/database');
 //Save the log details
-const LOGGER = require(path.resolve('.') + '/src/Logger/logger.js')
-const FILE_NAME = 'server.js'
+const LOGGER = require('./src/Logger/logger.js');
+const FILE_NAME = 'server.js';
 //Port to expose
-const port = 3000
-const fs = require('fs')
-//import the basic routes folder
-const basicRoutes = require(path.resolve('.') + '/src/Routes/basicroutes.js')
-//import the user routes
-const userRoutes = require(path.resolve('.') + '/src/Routes/userRoutes.js')
-const jobPostingRoutes = require(path.resolve('.') +
-  '/src/Routes/jobPostingRoutes.js')
-//import the job applications route
-const jobApplicationsRoutes = require(path.resolve('.') +
-  '/src/Routes/jobApplicationsRoutes.js')
-//import the email routes
-const emailRoutes = require(path.resolve('.') + '/src/Routes/emailRoutes.js')
+const port = 3000;
+//import the routes
+const basicRoutes = require('./src/Routes/basicroutes.js');
+const userRoutes = require('./src/Routes/userRoutes.js');
+const jobPostingRoutes = require('./src/Routes/jobPostingRoutes.js');
+const jobApplicationsRoutes = require('./src/Routes/jobApplicationsRoutes.js');
+const emailRoutes = require('./src/Routes/emailRoutes.js');
+const passwordRoutes = require('./src/Routes/passwordRoutes.js');
 
-//import the email routes
-const passwordRoutes = require(path.resolve('.') +
-  '/src/Routes/passwordRoutes.js')
+const mount = async (app) => {
+  // Open connection to DB
+  await connectDatabase(FILE_NAME);
+  // Init middleware
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(cors());
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL_DEV
-var ca = [fs.readFileSync(process.env.RDS_FILE)]
-//mongoose connection
-mongoose.Promise = global.Promise
-mongoose.connect(
-  MONGO_DB_URL_TEST,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    sslValidate: true,
-    sslCA: ca
-  },
-  (err, db) => {
-    if (err) {
-      CONSTANTS.createLogMessage(FILE_NAME, err, 'MONGODBCONNECTIONERROR')
-    } else {
-      CONSTANTS.createLogMessage(
-        FILE_NAME,
-        'Connection established to ' + MONGO_DB_URL,
-        'MONGODBCONNECTIONSUCCESS'
-      )
-    }
-  }
-)
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(cors())
+  // Wire up routes
+  app.use('/', basicRoutes);
+  app.use('/dev/user', userRoutes);
+  app.use('/dev/jobPosting', jobPostingRoutes);
+  app.use('/dev/jobApplication', jobApplicationsRoutes);
+  app.use('/dev/email', emailRoutes);
+  app.use('/dev/password', passwordRoutes);
 
-//basic route
-app.use('/', basicRoutes)
-//Routes for users
-app.use('/dev/user', userRoutes)
-//Routes for job Postings
-app.use('/dev/jobPosting', jobPostingRoutes)
-//Routes for job Applications
-app.use('/dev/jobApplication', jobApplicationsRoutes)
-//Routes for email
-app.use('/dev/email', emailRoutes)
-//Routes for password reset
-app.use('/dev/password', passwordRoutes)
+  app.listen(port, function () {
+    LOGGER.debug('Express server listening on port %s.', port);
+  });
+};
 
-app.listen(port, function () {
-  LOGGER.debug('Express server listening on port %s.', port)
-})
-module.exports = app
+mount(express());
--- src/temp/seed/index.js
+++ src/temp/seed/index.js
@@ -1,86 +1,86 @@
-const mongoose = require('mongoose')
-const CONSTANTS = require('../../CONSTANTS/constants')
-const JobPostingSchema = require('../../model/jobPostingModel')
-const UserSchema = require('../../model/userModel')
-const testJobs = require('./data/test_jobs.json')
-const testResearchers = require('./data/test_researchers.json')
-const testVolunteers = require('./data/test_volunteers.json')
+require('dotenv').config();
+const JobPostingSchema = require('../../model/jobPostingModel');
+const UserSchema = require('../../model/userModel');
+const testJobs = require('./data/test_jobs.json');
+const testResearchers = require('./data/test_researchers.json');
+const testVolunteers = require('./data/test_volunteers.json');
+const { connectDatabase } = require('../../database');
 
-const seedUsers = db => {
+const seedUsers = async (db) => {
   try {
-    const User = db.model('UserSchema', UserSchema)
+    const User = db.model('UserSchema', UserSchema);
 
     for (let researcher of testResearchers) {
-      const { _id, ...rest } = researcher
+      const { _id, ...rest } = researcher;
       let newUser = User({
         _id: _id['$oid'],
-        ...rest
-      })
-      newUser.save()
+        ...rest,
+      });
+      await newUser.save();
     }
 
     for (let volunteer of testVolunteers) {
-      const { _id, ...rest } = volunteer
+      const { _id, ...rest } = volunteer;
       let newUser = User({
         _id: _id['$oid'],
-        ...rest
-      })
-      newUser.save()
+        ...rest,
+      });
+      await newUser.save();
     }
   } catch (error) {
-    console.log(error)
+    throw error;
   }
-}
+};
 
-const seedJobs = db => {
+const seedJobs = async (db) => {
   try {
-    const JobPosting = db.model('JobPostingSchema', JobPostingSchema)
-    const [{ _id: researcherID1 }, { _id: researcherID2 }] = testResearchers
+    const JobPosting = db.model('JobPostingSchema', JobPostingSchema);
+    const [{ _id: researcherID1 }, { _id: researcherID2 }] = testResearchers;
 
     for (let i = 0; i < testJobs.length; i++) {
       // divvy up jobs between mock researchers
-      let currentResearcher = i % 2 ? researcherID1 : researcherID2
+      let currentResearcher = i % 2 ? researcherID1 : researcherID2;
 
       const {
         jobTitle,
         description,
         weeklyCommitment,
         postedDate,
-        skills
-      } = testJobs[i]
+        skills,
+      } = testJobs[i];
       let newPosting = new JobPosting({
         userID: currentResearcher['$oid'],
         jobTitle,
         description,
         weeklycommitment: weeklyCommitment,
         postedDate: postedDate['$date'],
-        skills
-      })
-      newPosting.save()
+        skills,
+      });
+      await newPosting.save();
     }
   } catch (error) {
-    console.log(error)
+    throw error;
   }
-}
+};
 
 const seed = async () => {
-  var ca = [fs.readFileSync('rds-combined-ca-bundle.pem')]
-  console.log('[SEED]: running...')
+  console.log('[SEED]: running...');
+  let db;
   try {
-    const db = await mongoose.connect(CONSTANTS.mongoDBUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      sslValidate: true,
-      sslCA: ca
-    })
+    db = await connectDatabase(__filename);
 
-    seedUsers(db)
-    seedJobs(db)
+    await seedUsers(db);
+    await seedJobs(db);
 
-    console.log('[SEED]: success')
+    console.log('[SEED]: success');
   } catch (error) {
-    console.log(error)
+    console.log(error.stack);
+    console.log('[SEED]: error');
+  } finally {
+    if (db) {
+      db.connection.close();
+    }
   }
-}
+};
 
-seed()
+seed();
